128ea57c386f5c8f62307839cc32041cb371abca
Danilo Batista de Souza <danxsouza@proton.me>
2024-08-12 23:38:18 -0300

    Implementing scenarios for valid and invalid searches on the blog

---
+++ tests/ui/input-search.spec.ts
@@ -0,0 +1,27 @@
+import { test } from '../myFixtures/testFixtures';
+
+function getRandom(array: string[]): string {
+    return array[Math.floor(Math.random() * array.length)];
+}
+const randomdata = ['pix', 'cartão', 'seguros'];
+const invalidData = ['-----', 'xxxOP@~~', '~//[][[]]'];
+
+const data = getRandom(randomdata);
+const invalid = getRandom(invalidData);
+
+
+test.beforeEach(' Visit Blog Page', async ({ page }) => {
+    await page.goto('/');
+});
+
+test('I Should fill input and search a value', async ({ blogPage }) => {
+    await blogPage.searchBlog(data);
+    await blogPage.checkTitleResult(data);
+    await blogPage.valueInserting(data);
+    await blogPage.checkArticleResult(data);
+});
+
+test('Checking an invalid search result and validate a message warning', async ({ blogPage }) => {
+    await blogPage.checkInvalidSearch(invalid);
+    await blogPage.valueInserting(invalid);
+});
---
+++ tests/utils/messages.ts
@@ -0,0 +1,8 @@
+export default {
+    validSearch: {
+        title: 'Resultados encontrados para: '
+    },
+    invalidSearch: {
+        message: 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.'
+    }
+}
